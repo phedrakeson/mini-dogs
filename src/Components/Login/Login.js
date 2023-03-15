cb19d079aee52dd43c5227e02b8568522949d336
Gabriel Duarte <gabrield.developer@gmail.com>
2023-03-15 00:29:22 -0300

    feat: creates basic project structure and implements login/logout component and actions

---
+++ src/Components/Login/Login.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useDispatch } from 'react-redux';
+import { autoLogin, login } from '../../store/login';
+import styles from './Login.module.css';
+
+const Login = () => {
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const dispatch = useDispatch();
+  React.useEffect(() => {
+    dispatch(autoLogin());
+  }, [dispatch])
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    dispatch(login({ username, password }));
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <label className={styles.label} htmlFor="username">Usuário</label>
+      <input className={styles.input} type="text" id="username" value={username} onChange={({ target }) => setUsername(target.value)} />
+
+      <label className={styles.label} htmlFor="password">Senha</label>
+      <input className={styles.input} type="password" id="password" value={password} onChange={({ target }) => setPassword(target.value)} />
+
+      <button className={styles.button}>Enviar</button>
+    </form>
+  )
+}
+
+export default Login
---
+++ src/store/login.js
@@ -0,0 +1,113 @@
+import { combineReducers } from "@reduxjs/toolkit";
+import createAsyncSlice from "../helper/createAsyncSlice";
+import getLocalStorage from "../helper/getLocalStorage";
+
+const token = createAsyncSlice({
+  name: 'token',
+  initialState: {
+    data: {
+      token: getLocalStorage('token', null)
+    }
+  },
+  reducers: {
+    removeToken(state) {
+      state.data = null;
+    },
+    fetchSuccess: {
+      reducer(state, action) {
+        state.loading = false;
+        state.data = action.payload;
+        state.error = null;
+      },
+      prepare(payload) {
+        return {
+          payload,
+          meta: {
+            localStorage: {
+              key: 'token',
+              value: payload.token
+            }
+          }
+        }
+      }
+    }
+  },
+  fetchConfig: (payload) => ({
+    url: 'https://dogsapi.origamid.dev/json/jwt-auth/v1/token',
+    options: {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload)
+    }
+  })
+})
+
+const user = createAsyncSlice({
+  name: 'user',
+  reducers: {
+    removeUser(state) {
+      state.data = null;
+    }
+  },
+  fetchConfig: (token) => ({
+    url: 'https://dogsapi.origamid.dev/json/api/user',
+    options: {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+    }
+  })
+})
+
+const reducer = combineReducers({ token: token.reducer, user: user.reducer });
+
+export const fetchToken = token.asyncAction;
+export const fetchUser = user.asyncAction;
+
+const { removeToken } = token.actions;
+const { removeUser } = user.actions;
+
+export const autoLogin = () => async (dispatch, getState) => {
+  const state = getState();
+  const { token } = state.login.token.data;
+  if (token) await dispatch(fetchUser(token));
+}
+
+export const login = (user) => async (dispatch) => {
+  try {
+    const { payload } = await dispatch(fetchToken(user));
+    if (payload.token !== undefined) await dispatch(fetchUser(payload.token));
+  } catch { }
+}
+
+export const userLogout = () => (dispatch) => {
+  dispatch(removeUser());
+  dispatch(removeToken());
+  window.localStorage.removeItem('token');
+}
+
+export default reducer;
+
+
+
+
+// export const fetchUser = (token) => async (dispatch) => {
+//   try {
+//     dispatch(fetchUserStarted());
+//     const response = await fetch('https://dogsapi.origamid.dev/json/api/user', {
+//       method: 'GET',
+//       headers: {
+//         Authorization: `Bearer ${token}`
+//       },
+//     });
+//     const data = await response.json();
+
+//     return dispatch(fetchUserSuccess(data));
+//   } catch (error) {
+//     dispatch(fetchUserError(error.message))
+//   }
+// };
+
